2c6cdf64146273481127b977d366d866a0056ce8
Daniel Steblin <d.steblin@googlemail.com>
2021-11-08 22:21:44 +0100

    finished encoding/imputing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+import math
+
 
 X_train = pd.read_csv('train.csv')
 X_test = pd.read_csv('test.csv')
@@ -10,6 +13,7 @@ X_test.set_index('Id',inplace=True)
 X_corr = X_train.corr()
 
 y = X_train.SalePrice
+m = y.size
 X_train.drop('SalePrice',axis=1,inplace=True)
 combine = pd.concat([X_train,X_test])
 
@@ -27,6 +31,9 @@ bsmtexposure_enc={'No':1,'Mn':2,'Av':3,'Gd':4}
 bsmtfintype_enc={'Unf':1,'LwQ':2,'Rec':3,'BLQ':4,'ALQ':5,'GLQ':6}
 functional_enc={'Sev':1,'Maj2':2,'Maj1':3,'Mod':4,'Min2':5,'Min1':6,'Typ':7}
 garagefinish_enc={'Unf':1,'RFn':2,'Fin':3}
+paved_enc={'N':0,'P':1,'Y':2}
+poolqc_enc={'Fa':1,'TA':2,'Gd':3,'Ex':4}
+fence_enc={'MnWw':1,'GdWo':2,'MnPrv':3,'GdPrv':4}
 combine['LotShape'] = combine['LotShape'].map(lotshape_enc)
 combine['LandSlope'] = combine['LandSlope'].map(landslope_enc)
 combine['ExterQual'] = combine['ExterQual'].map(exter_enc)
@@ -38,12 +45,15 @@ combine['BsmtFinType1'] = combine['BsmtFinType1'].map(bsmtfintype_enc).apply(lam
 combine['BsmtFinType2'] = combine['BsmtFinType2'].map(bsmtfintype_enc).apply(lambda x: impute_nan(x))
 combine['HeatingQC'] = combine['HeatingQC'].map(exter_enc)
 combine['CentralAir'] = combine['CentralAir'].apply(lambda x: 1 if x=='Y' else 0)
-combine['KitchenQual'] = combine['KitchenQual'].map(exter_enc).apply(lambda x: 3 if pd.isnull(x) else x).astype(int)
+combine['KitchenQual'] = combine['KitchenQual'].map(exter_enc).apply(lambda x: 3 if pd.isnull(x) else x)
 combine['Functional'] = combine['Functional'].map(functional_enc)
 combine['FireplaceQu'] = combine['FireplaceQu'].map(exter_enc).apply(lambda x: impute_nan(x))
 combine['GarageFinish'] = combine['GarageFinish'].map(garagefinish_enc)
 combine['GarageQual'] = combine['GarageQual'].map(exter_enc)
 combine['GarageCond'] = combine['GarageCond'].map(exter_enc)
+combine['PavedDrive'] = combine['PavedDrive'].map(paved_enc)
+combine['PoolQC'] = combine['PoolQC'].map(poolqc_enc).apply(lambda x: impute_nan(x))
+combine['Fence'] = combine['Fence'].map(fence_enc).apply(lambda x: impute_nan(x))
 
 ## Imputing
 combine['MSZoning'] = SimpleImputer(strategy='most_frequent').fit_transform(combine[['MSZoning']])
@@ -72,7 +82,7 @@ masvnrarea_dict = {'BrkCmn':masvnrarea_list[0],'BrkFace':masvnrarea_list[1],'Non
 combine['MasVnrArea'] = combine['MasVnrArea'].where(combine['MasVnrArea'].notnull(),other=combine['MasVnrType'].map(masvnrarea_dict))
 combine[['BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF']] = combine[['BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF']].applymap(lambda x: impute_nan(x))
 combine['Electrical'] = SimpleImputer(strategy='most_frequent').fit_transform(combine[['Electrical']])
-combine[['BsmtFullBath','BsmtHalfBath']] = combine[['BsmtFullBath','BsmtHalfBath']].applymap(lambda x: impute_nan(x)).astype(int)
+combine[['BsmtFullBath','BsmtHalfBath']] = combine[['BsmtFullBath','BsmtHalfBath']].applymap(lambda x: impute_nan(x))
 combine['Functional'] = SimpleImputer(strategy='most_frequent').fit_transform(combine[['Functional']])
 combine['GarageYrBlt'] = combine['GarageYrBlt'].where(pd.notnull(combine['GarageYrBlt']) | pd.isnull(combine['GarageType']), other=combine['YearBuilt'])
 combine['GarageYrBlt'] = combine['GarageYrBlt'].apply(lambda x: impute_nan(x))
@@ -84,6 +94,7 @@ combine['GarageQual'] = combine['GarageQual'].where(combine['GarageYrBlt']!=0,ot
 combine['GarageQual'] = combine['GarageQual'].apply(lambda x: 3 if pd.isnull(x) else x)
 combine['GarageCond'] = combine['GarageCond'].where(combine['GarageYrBlt']!=0,other=0)
 combine['GarageCond'] = combine['GarageCond'].apply(lambda x: 3 if pd.isnull(x) else x)
+combine['SaleType'] = SimpleImputer(strategy='most_frequent').fit_transform(combine[['SaleType']])
 
 ## Dropping Features
 combine.drop(['LotFrontage_Imputer','Utilities','Condition2'],axis=1,inplace=True)
@@ -91,47 +102,24 @@ combine.drop(['LotFrontage_Imputer','Utilities','Condition2'],axis=1,inplace=Tru
 combine.info()
 
 ## Label Encoding
-# combine = pd.get_dummies(combine,dummy_na=True,columns=['Alley','GarageType'])
-# combine = pd.get_dummies(combine,columns=['MSSubClass','MSZoning','LandContour','LotConfig','Neighborhood',
-# 'Condition1','BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType',
-# 'Foundation','Heating','Electrical'])
+combine = pd.get_dummies(combine,dummy_na=True,columns=['Alley','GarageType','MiscFeature'])
+combine = pd.get_dummies(combine,columns=['MSSubClass','MSZoning','LandContour','LotConfig','Neighborhood',
+'Condition1','BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType',
+'Foundation','Heating','Electrical','MoSold','SaleType','SaleCondition'])
 
 combine_corr = combine.corr()
-print(combine['GarageCond'].value_counts())
-print(combine['GarageQual'].loc[pd.isnull(combine['GarageQual']) & pd.notnull(combine['GarageType'])])
 
-# MSZoning
-## LotFrontage
-## Alley
-# Utilities
-# Exterior1st
-# Exterior2nd
-## MasVnrType
-## MasVnrArea
-## BsmtQual
-## BsmtCond
-## BsmtExposure
-## BsmtFinType1
-# BsmtFinSF1
-## BsmtFinType2
-# BsmtFinSF2
-# BsmtUnfSF
-# TotalBsmtSF
-## Electrical
-# BsmtFullBath
-# BsmtHalfBath
-# KitchenQual
-# Functional
-## FireplaceQu
-## GarageType
-## GarageYrBlt
-## GarageFinish
-# GarageCars
-# GarageArea
-## GarageQual
-## GarageCond
-## PoolQC
-## Fence
-## MiscFeature
-# SaleType
+X_train = combine.iloc[:m]
+X_test = combine.iloc[m:]
+
+log_X_train = X_train.applymap(lambda x: math.log10(1+x))
+log_y = y.apply(lambda x: math.log10(1+x))
+
+X_train.info()
+
+model_1 = LinearRegression()
+model_1.fit(log_X_train,log_y)
+cv_error_LR = cross_val_score(model_1,log_X_train,log_y,cv=5,scoring='neg_mean_squared_error').mean()
+print((X_train<0).sum(axis=1).sum())
+
 
